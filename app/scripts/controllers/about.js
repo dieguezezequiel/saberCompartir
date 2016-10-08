9054fcc4c81b5ca4bd23de81f4538f1374f70925
fedderamirezz <fedderamirezz@gmail.com>
2016-10-08 18:57:59 -0300

    Front End andando

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name saberCompartirApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the saberCompartirApp
+ */
+angular.module('saberCompartirApp')
+  .controller('AboutCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
