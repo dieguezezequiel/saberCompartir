043bf3b110375e706c82901b7edb8157f60887ce
MatiasDG <matiasdanielgarcia@gmail.com>
2016-10-07 21:44:11 -0300

    Se genera base del backend

---
+++ backend/src/main/java/com/sabercompartir/SaberCompartirApplication.java
@@ -0,0 +1,12 @@
+package com.sabercompartir;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SaberCompartirApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SaberCompartirApplication.class, args);
+	}
+}
