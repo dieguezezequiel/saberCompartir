57935d59afbd45a909e5c42455562731b0119b1b
fedderamirezz <fedderamirezz@gmail.com>
2016-10-09 18:23:19 -0300

    angular ui.router ANDANDO

---
+++ frontend/app/scripts/app.js
@@ -0,0 +1,37 @@
+'use strict';
+
+/**
+ * @ngdoc overview
+ * @name frontendApp
+ * @description
+ * # frontendApp
+ *
+ * Main module of the application.
+ */
+angular
+  .module('frontendApp', [
+    'ngAnimate',
+    'ngCookies',
+    'ngResource',
+    'ngRoute',
+    'ngSanitize',
+    'ngTouch',
+    'ui.router'
+  ])
+  .config(['$stateProvider','$urlRouterProvider',function($stateProvider,$urlRouterProvider){
+    $urlRouterProvider
+      .otherwise('/');
+
+    $stateProvider
+      .state('inicio',{
+        url:'/',
+        templateUrl: 'views/inicio.html',
+        controller: 'InicioCtrl'
+      })
+      .state('registro',{
+        url:'/registro',
+        templateUrl: 'views/registro.html',
+        controller: 'RegistroCtrl'
+      });
+
+  }]);
