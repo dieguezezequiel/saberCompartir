155b2f518a5d3c72da0709d48f28102764f3433c
MatiasDG <matiasdanielgarcia@gmail.com>
2016-10-11 18:55:24 -0300

    WIP home sweet home

--- frontend/app/scripts/controllers/inicio.js
+++ frontend/app/scripts/controllers/inicio.js
@@ -8,10 +8,13 @@
  * Controller of the frontendApp
  */
 angular.module('frontendApp')
-  .controller('InicioCtrl', function () {
+  .controller('InicioCtrl', ['$scope', function ($scope) {
     this.awesomeThings = [
       'HTML5 Boilerplate',
       'AngularJS',
       'Karma'
     ];
-  });
+
+    $scope.streamingActuales = [1,2,3,4,5,6,7,8];
+
+  }]);
