fdd867dd4ed1c8446ab64a318ed9211a5231b9eb
fedderamirezz <fedderamirezz@gmail.com>
2016-10-15 11:51:48 -0300

    Toqueteo del navbar

--- frontend/Gruntfile.js
+++ frontend/Gruntfile.js
@@ -70,10 +70,10 @@ module.exports = function (grunt) {
     // The actual grunt server settings
     connect: {
       options: {
-        port: 9001,
+        port: 9000,
         // Change this to '0.0.0.0' to access the server from outside.
         hostname: 'localhost',
-        livereload: 357291
+        livereload: 35729
       },
       livereload: {
         options: {
@@ -100,7 +100,7 @@ module.exports = function (grunt) {
       },
       test: {
         options: {
-          port: 9001,
+          port: 9000,
           middleware: function (connect) {
             return [
               connect.static('.tmp'),
