956e98b46907d0ab40459aeb76b7d5913456d2dc
MatiasDG <matiasdanielgarcia@gmail.com>
2016-10-29 03:22:05 -0300

    WIP Dockerizacion

--- docker/backend/build_backend_docker.sh
+++ docker/backend/build_backend_docker.sh
@@ -1,9 +1,11 @@
 #!/bin/sh
-cd ../backend
+cd ../../backend
 echo "Construyendo el proyecto SaberCompartir backend"
 gradle clean
 gradle build
+sudo cp build/libs/saberCompartir-0.0.1-SNAPSHOT.jar ../docker/backend/saberCompartir-0.0.1-SNAPSHOT.jar
+cd ../docker/backend
 echo "Construyendo la imagen matiasdg/sabercompartir_backend"
-docker build --no-cache --rm=true -t matiasdg/sabercompartir_backend .
+sudo docker build --no-cache --rm=true -t matiasdg/sabercompartir_backend .
 echo "Publicando la imagen matiasdg/sabercompartir_backend"
-docker push matiasdg/sabercompartir_backend
+sudo docker push matiasdg/sabercompartir_backend
--- docker/frontend/build_frontend_docker.sh
+++ docker/frontend/build_frontend_docker.sh
@@ -1,11 +1,13 @@
 #!/bin/sh
-cd ../frontend
+cd ../../frontend
 echo "Construyendo el proyecto SaberCompartir frontend"
 grunt clean
 grunt build
 if [ $? -ne 0 ];
     then exit 1
 fi
+sudo cp dist ../docker/frontend/dist
+cd ../docker/frontend
 echo "Construyendo la imagen matiasdg/sabercompartir_frontend"
 docker build --no-cache --rm=true -t matiasdg/sabercompartir_frontend .
 echo "Publicando la imagen matiasdg/sabercompartir_frontend"
