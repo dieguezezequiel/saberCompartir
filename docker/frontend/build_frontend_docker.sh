1ba54425a847aa8a05baf32b11c933c1efec5ea8
MatiasDG <matiasdanielgarcia@gmail.com>
2016-10-29 23:02:00 -0300

    WIP Dockerizacion

--- docker/frontend/build_frontend_docker.sh
+++ docker/frontend/build_frontend_docker.sh
@@ -1,14 +1,14 @@
 #!/bin/sh
 cd ../../frontend
 echo "Construyendo el proyecto SaberCompartir frontend"
-grunt clean
-grunt build
+sudo grunt clean
+sudo grunt build
 if [ $? -ne 0 ];
     then exit 1
 fi
-sudo cp dist ../docker/frontend/dist
+sudo cp -rf dist ../docker/frontend/dist
 cd ../docker/frontend
 echo "Construyendo la imagen matiasdg/sabercompartir_frontend"
-docker build --no-cache --rm=true -t matiasdg/sabercompartir_frontend .
+sudo docker build --no-cache --rm=true -t matiasdg/sabercompartir_frontend .
 echo "Publicando la imagen matiasdg/sabercompartir_frontend"
-docker push matiasdg/sabercompartir_frontend
+sudo docker push matiasdg/sabercompartir_frontend
