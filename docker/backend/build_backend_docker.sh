0d7619676dd7142a7fae93d5015325d340521b2c
EC2 Default User <ec2-user@ip-172-31-18-137.us-east-2.compute.internal>
2017-11-05 01:07:28 +0000

    Cambio en buil-backend-docker y en application.yml

--- docker/backend/build_backend_docker.sh
+++ docker/backend/build_backend_docker.sh
@@ -6,6 +6,6 @@ gradle build
 sudo cp build/libs/saberCompartir-0.0.1-SNAPSHOT.jar ../docker/backend/saberCompartir-0.0.1-SNAPSHOT.jar
 cd ../docker/backend
 echo "Construyendo la imagen matiasdg/sabercompartir_backend"
-sudo docker build --no-cache --rm=true -t matiasdg/sabercompartir_backend .
+docker build --no-cache --rm=true -t matiasdg/sabercompartir_backend .
 echo "Publicando la imagen matiasdg/sabercompartir_backend"
-sudo docker push matiasdg/sabercompartir_backend
+docker push matiasdg/sabercompartir_backend
